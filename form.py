93bef72faff718231ff10dc61e7a744b18451698
Christelle <114661244+chrichri156@users.noreply.github.com>
2022-12-30 15:45:20 +0100

    Update form.py

--- form.py
+++ form.py
@@ -1,35 +1,35 @@
 import mysql.connector
 
-again3 = "Yes"
-while again3 == "Yes":
+repeat_form = "Yes"
+while repeat_form == "Yes":
+    firstNameStudent = input ("Insert your first name:")
+    lastNameStudent = input ("Insert your last name:")
+    classStudent = input ("Insert your class:")
+    email = input ("Insert your email address:")
+
+    print(f"Hello {firstNameStudent} {lastNameStudent}, from {classStudent}. Your email address is: {email}")
 
 
-    again1 = "Yes"
-    while again1 == "Yes":
-        firstNameStudent = input ("Insert your first name:")
-        lastNameStudent = input ("Insert your last name:")
-        classStudent = input ("Insert your class:")
-        email = input ("Insert your email address:")
-
-        print(f"Hello {firstNameStudent} {lastNameStudent}, from {classStudent}. Your email address is: {email}")
-
-
-        again1 = input ("Do you want to change those informations (Yes/No) ? ")
+    change_info = input ("Do you want to change those informations (Yes/No) ? ")
     
+    if change_info == "Yes":
+        continue
+    elif change_info == "No":
+        break
+    else:
+        print("Invalid input. Please enter 'Yes' or 'No'.")
 
-    again2 = "Yes"
-    while again2 == "Yes":
+    repeat_form = "Yes"
+    while repeat_form == "Yes":
     
         presence = input ("Will you be present at the Prom Ball on the 28th of June 2023? (Yes/No)") 
     
         if presence == "Yes":
-    
             guest = input ("Will you be accompanied (maximum 1 guest) (Yes/No) ? ")
         
             if guest == "Yes":
-    
-                firstNameGuest = input ("If yes, insert your guest’s first name:")
-                lastNameGuest = input ("If yes, insert your guest’s last name:")
+                guest_firstname = input ("If yes, insert your guest’s first name:")
+                guest_lastname = input ("If yes, insert your guest’s last name:")
         
                 print(f"It's great that you're coming! Your guest is {firstNameGuest} {lastNameGuest}")
             
@@ -37,14 +37,26 @@ while again3 == "Yes":
                 print("It's great that you're coming! You have no guest for the moment.")
                 
             
-        else:
+        elif presence == "No":
             print("Too bad... We'll see you another time, then!")
         
-        again2 = input ("Do you want to change those informations (Yes/No) ? ")
+        else:
+            print("Invalid input. Please enter 'Yes' or 'No'.")
+        
+        change_info = input("Do you want to change those informations (Yes/No) ? ")
+        if change_info == "Yes":
+            continue
+        
+        elif change_info == "No":
+            break
+        
+        else:
+            print("Invalid input. Please enter 'Yes' or 'No'.")
 
 
 
-    if presence == "Yes" and guest == "Yes":
+    if presence == "Yes"
+        if guest == "Yes":
          print("The total amount of your order is (25€/pp):", 2*25 , "€")
         
     elif presence == "Yes" and guest == "No":
