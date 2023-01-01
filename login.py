74e03cf7eda72313570035fec3315f39d4f7d0a0
Christelle <114661244+chrichri156@users.noreply.github.com>
2023-01-01 16:55:00 +0100

    Delete login.py

--- login.py
+++
@@ -1,9 +0,0 @@
-username = input("Enter your username: ")
-password = input("Enter your password: ")
-
-while username != "chrichri156" or password != "PassWord123":
-    print("Either you have made a mistake in your username and/or password, or you do not have access to the admin account.")
-    username = input("Enter your username: ")
-    password = input("Enter your password: ")
-
-print(f"Welcome {username}. You are well logged in to the admin account.")
